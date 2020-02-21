47550b007f6eaf2d1dcc78bdca7f7090e8a65dbb
Remember-Them <1439464054@qq.com>
2020-02-21 11:41:13 +0800

    数据填充

--- js/index.js
+++ js/index.js
@@ -1,18 +1,51 @@
-$(function (){
-    
+$(function () {
+    userName();
+    className();
 
-    var uname=localStorage.getItem('USERID');
-    // get_value_from_storage('USERID');
-    $('.uname').html(uname);
-    
-    $(document).on('click','.class-list li',function(){
+
+
+    function userName() {
+        var uname = get_value_from_storage('USERID');
+        $('.uname').html(uname);
+    }
+
+
+    function className() {
+        var classname = ['移动2班', '软信2班', '网销2班'];
+        var liContent = '';
+        for (var i = 0; i < classname.length; i++) {
+            liContent += '<li>' + classname[i] + '</li>';
+        }
+        $('.class-list').html(liContent);
+
+    }
+
+    $(document).on('click', '.class-list li', function () {
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
+        $('.right-class').html($(this).html());
     })
-    var classname=['移动2班','软信2班','网销2班'];
-    var liContent= '';
-    for(var i = 0;i < classname.length;i++){
-        liContent+='<li>'+classname[i]+'</li>';
+
+    var studentArr = students_json_01.data;
+    var studentContent = '';
+    for (var i = 0; i < studentArr.length; i++) {
+        studentContent +=
+            "<tr data-index=" + i + ">" +
+            "<td><input type='checkbox'></td>" +
+            "<td>" + studentArr[i].truename + "</td>" +
+            "<td>" + studentArr[i].sno + "</td>" +
+            "<td>" + studentArr[i].tel + "</td>" +
+            "<td>" + studentArr[i].gender + "</td>" +
+            "<td>" + studentArr[i].age + "</td>" +
+            "<td>" + studentArr[i].major + "</td>" +
+            "<td>" +
+            "<button class='btn btn-edit btn-info'>编辑</button>" +
+            "<button class='btn btn-del btn-danger'>删除</button>" +
+            "</td>" +
+            "</tr>"
+        $('.student-info').html(studentContent);
+        $(document).on('click', '.btn-edit', function () {
+            console.log($(this).parents('tr').attr('data-index'))
+        })
     }
-    $('.class-list').html(liContent);
 })
